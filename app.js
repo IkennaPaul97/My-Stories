a949d294bf161c9c452786ee8c4366fc045c30b9
Ikenna Paul Abonyi <75263399+IkennaPaul97@users.noreply.github.com>
2022-01-10 22:29:25 +0100

    Create STories/ Display Stories in Dashboard/hbs Helpers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars');
 const passport = require('passport')
 const session = require('express-session')
+const MongoDbStore = require('connect-mongo');
 const connectDB = require('./config/db')
 
 //Load config
@@ -17,14 +19,23 @@ connectDB()
 
 const app = express()
 
+//Body Parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
 //logging
 if (process.env.NODE_ENV === 'development')
   {
       app.use(morgan('dev'))
   }
 
+//Handlebars Helpers
+const { formatDate } = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', engine({ helpers: {
+  formatDate, 
+}, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -33,6 +44,9 @@ app.use(
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
+  store: MongoDbStore.create({
+    mongoUrl: process.env.MONGO_URL,
+})
 }))
 
 
@@ -48,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 5000
 
